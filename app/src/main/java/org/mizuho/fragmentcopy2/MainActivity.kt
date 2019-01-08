a6f935fb90b72f0478714c4df8b64c121b2a04f6
田所みづほ <mizuho.tadokoro@nijibox.co.jp>
2019-01-08 12:10:33 +0900

    finish copying

--- app/src/main/java/org/mizuho/fragmentcopy2/MainActivity.kt
+++ app/src/main/java/org/mizuho/fragmentcopy2/MainActivity.kt
@@ -2,11 +2,16 @@ package org.mizuho.fragmentcopy2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = FirstImageFragment.newInstance()
+
+
     }
 }
---
+++ app/src/main/java/org/mizuho/fragmentcopy2/MyAppGlideModule.kt
@@ -0,0 +1,6 @@
+package org.mizuho.fragmentcopy2
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class MyAppGlideModule : AppGlideModule()
